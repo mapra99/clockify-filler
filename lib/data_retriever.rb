718b64e1a5f16a5c8760d3acaa11496817d5f8c0
Miguel Prada <mapra99@gmail.com>
2019-12-05 16:11:54 -0500

    refactord data_retriever to create files directories

--- lib/data_retriever.rb
+++ lib/data_retriever.rb
@@ -7,23 +7,29 @@ module DataRetriever
   include RequestBuilder
 
   def workspaces(opts = {})
-    if !(File.file? './data/workspaces.json') || opts[:reload]
+    Dir.mkdir("./data") unless File.exist?('./data')
+    file_path = './data/workspaces.json'
+
+    if !(File.file? file_path) || opts[:reload]
       puts "\nRetrieving workspaces from API"
       response = RequestBuilder.new_get_request '/workspaces'
-      File.open('./data/workspaces.json', 'w') { |file| file.puts response.body }
+      File.open(file_path, 'w') { |file| file.puts response.body }
     end
 
-    file = File.open('./data/workspaces.json', 'r')
+    file = File.open(file_path, 'r')
     JSON.load file
   end
 
   def projects(opts)
-    if !(File.file? "./data/#{opts[:workspace_id]}_projects.json") || opts[:reload]
+    Dir.mkdir("./data/#{opts[:workspace_id]}") unless File.exist?("./data/#{opts[:workspace_id]}")
+    file_path = "./data/#{opts[:workspace_id]}/projects.json"
+
+    if !(File.file? file_path) || opts[:reload]
       puts "\nRetrieving workspace projects from API"
       response = RequestBuilder.new_get_request "/workspaces/#{opts[:workspace_id]}/projects"
-      File.open("./data/#{opts[:workspace_id]}_projects.json", 'w') { |file| file.puts response.body }
+      File.open(file_path, 'w') { |file| file.puts response.body }
     end
-    file = File.open("./data/#{opts[:workspace_id]}_projects.json", 'r')
+    file = File.open(file_path, 'r')
     JSON.load file
   end
 end
