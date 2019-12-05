fae486a3725ece70a139139491d7e0d205080fd2
Miguel Prada <mapra99@gmail.com>
2019-12-05 15:45:28 -0500

    created method to retrieve projects

--- lib/data_retriever.rb
+++ lib/data_retriever.rb
@@ -8,7 +8,7 @@ module DataRetriever
 
   def workspaces(opts = {})
     if !(File.file? './data/workspaces.json') || opts[:reload]
-      puts 'retrieving workspaces from API'
+      puts "\nRetrieving workspaces from API"
       response = RequestBuilder.new_get_request '/workspaces'
       File.open('./data/workspaces.json', 'w') { |file| file.puts response.body }
     end
@@ -17,5 +17,13 @@ module DataRetriever
     JSON.load file
   end
 
-  def projects(workspace); end
+  def projects(opts)
+    if !(File.file? "./data/#{opts[:workspace_id]}_projects.json") || opts[:reload]
+      puts "\nRetrieving workspace projects from API"
+      response = RequestBuilder.new_get_request "/workspaces/#{opts[:workspace_id]}/projects"
+      File.open("./data/#{opts[:workspace_id]}_projects.json", 'w') { |file| file.puts response.body }
+    end
+    file = File.open("./data/#{opts[:workspace_id]}_projects.json", 'r')
+    JSON.load file
+  end
 end
